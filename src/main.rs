11e7cd57aaf28f9abb827bded97ceee3e630bcae
Platon Floria <platon.floria@gmail.com>
2023-08-08 21:10:18 +0800

    feat: how to use section

--- src/main.rs
+++ src/main.rs
@@ -68,7 +68,6 @@ fn App() -> Html {
                                 allowfullscreen=true
                             />
                             <div style="overflow=scroll">
-
                                 <p>{ "The Mandelbrot set is a mathematical concept that can be visualized on a Cartesian plane with coordinates ranging from -2 to 2 on both the x and y axes." }</p>
                                 <br/>
                                 <p>{ "In our system, each NFT (Non-Fungible Token) represents a specific region within these coordinates, defined by a rectangular shape. We have developed custom software that generates a graphical representation for each NFT." }</p>
@@ -86,6 +85,62 @@ fn App() -> Html {
                                 <p>{ "The owner of a parent NFT can set a minimum amount of FUEL that must be used in a mint bid. All child NFTs created within that parent NFT must adhere to this minimum requirement." }</p>
                                 <br/>
                                 <p>{ "Owner of an NFT can burn it, given that it doesn't have any child NFTs in it. When an NFT is burned, FUEL that was locked inside of it is transferred to its owner." }</p>
+                            </div>
+                        </Tab>
+                        <Tab label="How to Use">
+                            <div style="overflow=scroll">
+                                <h1>{ "Welcome to Mandelbrot NFT!" }</h1>
+                                <p>{ "This decentralized application allows you to interact with the Mandelbrot NFT ecosystem and create, trade, and
+                                explore unique visual representations of the Mandelbrot set. This guide will walk you through the main functionalities
+                                and steps to get started." }</p>
+                                <br/>
+                                <h2>{ "Prerequisites:" }</h2>
+                                <ol>
+                                <li>{ "You will need a compatible web browser with a web3 wallet extension (such as MetaMask) installed." }</li>
+                                <li>{ "Make sure you have some wFUEL tokens in your web3 wallet to pay for minting NFTs." }</li>
+                                </ol>
+                                <br/>
+                                <h2>{ "Getting Started:" }</h2>
+
+                                <h3>{ "Step 1: Accessing Mandelbrot NFT" }</h3>
+                                <ol>
+                                <li>{ "Open your web browser and navigate to https://mandelbrot-nft.onrender.com." }</li>
+                                <li>{ "Ensure that your web3 wallet extension is active and connected to the Sepolia network." }</li>
+                                </ol>
+                                <br/>
+                                <h3>{ "Step 2: Connect your Wallet" }</h3>
+                                <ol>
+                                <li>{ "On the dApp interface, click the 'Connect Wallet' button." }</li>
+                                <li>{ "Follow the prompts from your web3 wallet extension to connect it to the dApp." }</li>
+                                </ol>
+                                <br/>
+                                <h3>{ "Step 3: Buy wrapper FUEL" }</h3>
+                                <ol>
+                                <li>{ "On the dApp interface, click the 'Buy wFUEL' button." }</li>
+                                <li>{ "You will be redirected to the Uniswap pair where you will have an opportunity to buy wFUEL." }</li>
+                                </ol>
+                                <br/>
+                                <h3>{ "Step 4: Unwrap wrapper FUEL" }</h3>
+                                <ol>
+                                <li>{ "On the dApp interface, select the amount of wFUEL that you wish to unwrap and click the 'Unwrap' button." }</li>
+                                <li>{ "Once the transaction succeeds your balance will be refreshed." }</li>
+                                </ol>
+                                <br/>
+                                <h3>{ "Step 5: Explore the Mandelbrot Set" }</h3>
+                                <ol>
+                                <li>{ "You can pan and zoom on the Cartesian plane to explore different regions of the Mandelbrot set." }</li>
+                                <li>{ "Each red or blue frame represents an NFT, and you can double click on any NFT to view its details." }</li>
+                                </ol>
+                                <br/>
+                                <h3>{ "Step 6: Minting NFTs" }</h3>
+                                <ol>
+                                <li>{ "To mint an NFT within the coordinates of an existing NFT, double click on the NFT of interest." }</li>
+                                <li>{ "On the NFT details page, click the 'Bid' button." }</li>
+                                <li>{ "Set the amount of FUEL you are willing to spend on the minting process." }</li>
+                                <li>{ "Set the minimum bid amount needed for others to mint NFTs inside of your's." }</li>
+                                <li>{ "Afterwards submit your bid, it will be represented as a yellow frame." }</li>
+                                <li>{ "The owner of the parent NFT will review the bids and decide which NFTs get minted." }</li>
+                                </ol>
                             </div>
                         </Tab>
                     </Tabs>
