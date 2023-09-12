536c0ccf8c5b4674355f60785c0face063b2a472
Platon Floria <plfloria.floria@gmail.com>
2023-09-12 20:59:40 +0800

    feat: color offset control

--- src/components/auction.rs
+++ src/components/auction.rs
@@ -19,12 +19,24 @@ pub fn Auction(
 ) -> impl IntoView {
     let mandelbrot = expect_context::<Arc<Mutex<mandelbrot_explorer::Interface>>>(cx);
     let (max_iterations, set_max_iterations) = create_signal(cx, 40.0);
+    let (offset, set_offset) = create_signal(cx, 0.0);
 
     create_effect(cx, {
         let mandelbrot = mandelbrot.clone();
         move |_| {
             let mut mandelbrot = mandelbrot.lock().unwrap();
             mandelbrot.coloring.max_iterations = (max_iterations() as f64).powi(2) as i32;
+            if let Some(redraw) = &mandelbrot.redraw {
+                redraw();
+            }
+        }
+    });
+
+    create_effect(cx, {
+        let mandelbrot = mandelbrot.clone();
+        move |_| {
+            let mut mandelbrot = mandelbrot.lock().unwrap();
+            mandelbrot.coloring.offset = offset() as f32;
             if let Some(redraw) = &mandelbrot.redraw {
                 redraw();
             }
@@ -62,9 +74,14 @@ pub fn Auction(
         set_bid_amount(token.get().minimum_price);
         set_bids_minimum_price(token.get().minimum_price);
         view! { cx,
+            "Max iterations"
             <Slider style="width: 35em" min=0.0 max=200.0
                 value=max_iterations set_value=set_max_iterations
-                value_display=create_callback(cx, move |v: f64| format!("{:.0}", v.powi(2))) />
+                value_display=create_callback(cx, move |v: f64| format!("{:.0}", v.powi(2)))/>
+            "Color offset"
+            <Slider style="width: 35em" min=0.0 max=1.0
+                value=offset set_value=set_offset
+                value_display=create_callback(cx, move |v: f64| format!("{v:.4}"))/>
             <Stack orientation=StackOrientation::Horizontal spacing=Size::Em(0.6)>
                 <Stack orientation=StackOrientation::Vertical spacing=Size::Em(0.6)>
                     <Stack orientation=StackOrientation::Horizontal spacing=Size::Em(0.6)>
